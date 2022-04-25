ffaae6b4b91e731e3db82b4f031a4cdb84ace3bb
Xilatau0 <davidjlopes0@gmail.com>
2022-04-25 20:33:40 +0100

    25-4-2022

--- index.php
+++ index.php
@@ -1,67 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+<head>
+<title>Table with database</title>
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+}
 
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
+</head>
+
+<body>
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "xilakicks";
-$conn = NEW mysqli($servername, $username, $password, $database);
-?>
+$conn = NEW Mysqli($servername, $username, $password, $database);
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-<style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-}
+$query = "SELECT * FROM sneakers INNER JOIN images ON sneakers.Modelo = images.Modelo;"; 
+$query_run = mysqli_query($conn, $query);
 
-.price {
-  color: grey;
-  font-size: 22px;
-}
-
-.card button {
-  border: none;
-  outline: 0;
-  padding: 12px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
-
-.card button:hover {
-  opacity: 0.7;
-}
-</style>
-</head>
-<body>
-
-  
+while($row = mysqli_fetch_array($query_run)){
+  ?>
 <div class="card">
-<?php
-$qry = mysqli_query($conn,"SELECT * FROM images");
-while ($row = mysqli_fetch_array($qry))
-{
-    echo "<img src=".$row['image_url']." />";
-}
-?>  
-  <h1>Tailored Jeans</h1>
-  <p class="price">$19.99</p>
-  <p>Some text about the jeans..</p>
+  <div class="container">
+  <img src="<?php echo $row['image_url']?> ." alt="Avatar" style="width:100%">
+      <h4><b> <?php echo $row['Modelo'] ?> </b></h4> 
+      <p> <?php echo $row['Marca'] ?> </p> 
+      <p> <?php echo $row['Valor'] ?> </p> 
+  </div>
 </div>
 
-
+<?php
+}
+?>
 
 </body>
-</html>
+</html> 
